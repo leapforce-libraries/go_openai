276a25bc36ef427cbaaf9f3ee2805e93a4946313
caselongo <caselongo@gmail.com>
2023-03-03 19:49:52 +0100

    Update completions.go

--- completions.go
+++ completions.go
@@ -7,10 +7,10 @@ import (
 )
 
 type CompletionsConfig struct {
-	Model       string `json:"model"`
-	Prompt      string `json:"prompt"`
-	Temperature int    `json:"temperature"`
-	MaxTokens   int    `json:"max_tokens"`
+	Model       string  `json:"model"`
+	Prompt      string  `json:"prompt"`
+	Temperature float64 `json:"temperature"`
+	MaxTokens   int     `json:"max_tokens"`
 }
 
 type CompletionsResponse struct {
